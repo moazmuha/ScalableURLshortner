fbe05d47a9ca006fd8aa76a6d59ea12565d66f44
student <aj.lee@mail.utoronto.ca>
2020-11-09 00:38:16 -0500

    PUT requests now work properly, working on GET requests - Hamza Khan

--- docker/app/app.py
+++ docker/app/app.py
@@ -38,50 +38,48 @@ def insert_cassandra(short_url, long_url):
 	INSERT INTO urlshorts (short, long)
 	VALUES (%s, %s);
 	"""
-	try:
-		session.execute(insert_statement, (short_url, long_url))
-		return True
-	except Exception:
-		return False
+	session.execute(insert_statement, (short_url, long_url))
 
 
 def get_cassandra(short_url):
-	return ''
 
+	get_statement = """
+	SELECT long from urlshorts
+	WHERE short=(%s);
+	"""
+	
+	return session.execute(get_statement, (short_url))
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'PUT'])
 def hello():
-	html = """ 
-<form>
-	<input type="text" name="short" />
-	<input type="text" name="long" />
-	<input type="submit" />
-</form>
-"""
-	html=""
-	
-	#if request.method == 'PUT':
-	args = request.args
-	
-	if 'short' in args and 'long' in args:
-		# Add to Cassandra
-		short_url = args['short']
-		long_url = args['long']
-		insert_cassandra(short_url, long_url)
-		return Response("{short_url:long_url}", status=200)
-	else:
-		return Response("{}", status=400)
-	
-	# Get from Redis
-		# if successful, return 307
 		
-	# If not in redis, get from cassandra and add to redis
-		# If successful, return 307
+	if request.method == 'PUT':
+		args = request.args
+	
+		if 'short' in args and 'long' in args:
+			# Add to Cassandra
+			short_url = args['short']
+			long_url = args['long']
+			
+			return Response('Succeeded\n', status=200)
+		else:
+			return Response('Invalid Format\n', status=400)
+	
+	if request.method == 'GET':
+		#Get from Redis
+			#if successful, return 307
+		
+		# If not in redis, get from cassandra and add to redis
+			# If successful, return 307
+		
+		#test = get_cassandra(request.full_path)
+		#return (test, status=307)
 
-	# If not in either, return 404
-
+		# If not in either, return 404
+		pass
+	
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80)
 
