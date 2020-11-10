71323f958fd7032a0db492f24f2856ddada05793
student <aj.lee@mail.utoronto.ca>
2020-11-10 06:24:09 -0500

    Working url shortner, with cassandra as database and redis as cache, also adds some scripts for orchestration - Hamza Khan

--- docker/app/app.py
+++ docker/app/app.py
@@ -23,15 +23,15 @@ profile = ExecutionProfile(
 cluster = Cluster(execution_profiles={EXEC_PROFILE_DEFAULT: profile})
 '''
 # # IPS : 10.11.12.20, 10.11.12.21, 10.11.12.22, 10.11.12.233, 10.11.12,234
-# cluster = Cluster(['10.11.12.20'])
+cluster = Cluster(['10.11.12.20', '10.11.12.21', '10.11.12.22', '10.11.12.233', '10.11.12.234'])
 # # session = cluster.connect()
-# #session = cluster.connect('a2')
-# session = cluster.connect('a2')
+session = cluster.connect('a2')
 
 # # session.set_keyspace('users')
 # # or you can do this instead
 # # session.execute('USE users')
 
+# Inserts the short and the long into the cassandra database
 def insert_cassandra(short_url, long_url):
 
 	insert_statement = """
@@ -40,72 +40,77 @@ def insert_cassandra(short_url, long_url):
 	"""
 	session.execute(insert_statement, (short_url, long_url))
 
-
+# Get the long associated with the given short from the cassandra
+# database and also stores the short and long into the redis database
 def get_cassandra(short_url):
 
-	get_statement = """
-	SELECT long from urlshorts
-	WHERE short=(%s);
+	select_statement = """
+	SELECT long FROM urlshorts
+	WHERE short=%s;
 	"""
 
-	return session.execute(get_statement, (short_url))
+	long_row = session.execute(select_statement, [short_url])
+	
+	try:
+		long_url = long_row[0].long
+		
+		if "http://" in long_url[0:7] or "https://" in long_url[0:8]:
+			long_url = long_url
+		else:
+			long_url = "http://" + long_url
+		
+		# Since short was not found in cache, store the short in cache
+		redis.set(short_url, long_url)
+		redis.expire(short_url, 180)
+		
+		return long_url
+	except:
+		return None
 
 app = Flask(__name__)
 
+# Put route to store the short and longs into the cassandra database
 @app.route("/", methods=['PUT'])
-def putRequest():
+def put_request():
 
 	if request.method == 'PUT':
 		args = request.args
+
+		# Checks if the url is valid
 		if 'short' in args and 'long' in args:
-			try:
-				if redis.exists(args['short']):
-					return Response("The provided short '{}' is already in use", 404)
-				redis.set(args['short'], args['long'])
-				#expire all keys 180 seconds after creation since this is the cache
-				redis.expire(args['short'],180)
-				return Response('PUT Succeeded {} now redirects to {}\n'.format(args['short'], args['long']), status=200)
-			except:
-				return Response("An error occured related to redis", 404)
-
-		# if 'short' in args and 'long' in args:
-		# 	# Add to Cassandra
-		# 	short_url = args['short']
-		# 	long_url = args['long']
-
-		# 	return Response('Succeeded\n', status=200)
-		# else:
-		# 	return Response('Invalid Format\n', status=400)
-
-
+			# Add to Cassandra
+			short_url = args['short']
+			long_url = args['long']
+			insert_cassandra(short_url, long_url)
+			return Response('Succeeded\n', status=200)
+		else:
+			return Response('Invalid Format\n', status=400)
+	
+# Get route to obtain the long associated with the given short	
 @app.route("/<short>", methods=['GET'])
-def getRequest(short):
-
+def get_request(short):
+	
 	if request.method == 'GET':
+		
+		# Checks to see if the short is within the cache
 		try:
 			if redis.exists(short):
-				short = redis.get(short)
-				if "http://" in short[0:7] or "https://" in short[0:8]:
-					redirectTo = short
-				else:
-					redirectTo = "http://"+short
-				return redirect(redirectTo, code=302)
-			else:
-				#check cassandra and add entry to redis with expiry. If not in cassandra give error below
-				return Response("The provided short: {} does not exist".format(short), 404)
+				long_url = redis.get(short)
+				return redirect(long_url, code=307)
+
 		except RedisError:
-			return Response("An error occured related to redis", 404)
-		#Get from Redis
-		#if successful, return 307
-
-		# If not in redis, get from cassandra and add to redis
-		# If successful, return 307
-
-		#test = get_cassandra(request.full_path)
-		#return (test, status=307)
-
-		# If not in either, return 404
-	
+			return Response("An error occurred related to redis", 404)
+		
+		# Retrieve the long if available
+		long_url = get_cassandra(short)
+		
+		# Checks to see if the short is in the main database and if its not
+		# returns a 404 error
+		if long_url:
+			return redirect(long_url, code=307)
+		else:
+			return Response('Page does not exist\n', status=404)
+		
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80, threaded=True)
 
